d5befb29afe1080424105589143d3f78d30a8e5d
DanielMiguelez <danielmiguelez1993@gmail.com>
2024-10-04 12:00:09 +0200

    home changes

--- src/app/tienda-videojuegos.service.ts
+++ src/app/tienda-videojuegos.service.ts
@@ -15,13 +15,7 @@ export class TiendaVideojuegosService {
     { name: 'Uncharted', type: 'Aventura', year: "2007", platform: 'PS3' },
     { name: 'Resident Evil', type: 'Terror', year: "1996", platform: 'PS1' },
     { name: 'Counter-Strike', type: 'FPS', year: "1999", platform: 'PC' },
-    { name: 'The Witcher 3', type: 'RPG', year: "2015", platform: 'PC, PS4, Xbox One' },
-    { name: 'Dark Souls', type: 'RPG', year: "2011", platform: 'PS3, Xbox 360' },
-    { name: 'The Legend of Zelda: Breath of the Wild', type: 'Aventura', year: "2017", platform: 'Switch' },
-    { name: 'Overwatch', type: 'FPS', year: "2016", platform: 'PC, PS4, Xbox One' },
-    { name: 'Silent Hill 2', type: 'Survival Horror', year: "2001", platform: 'PS2' },
-    { name: 'God of War', type: 'Aventura', year: "2005", platform: 'PS2' },
-    { name: 'Doom', type: 'FPS', year: "1993", platform: 'PC' }
+    
   ];
 
 
